04804ec04d6b8b3f04933f02075b1aaae4d8237b
Heloisa Rolins <heloisarolins@gmail.com>
2025-05-07 20:45:44 -0300

    outra parte da aula

---
+++ veiculo/forms.py
@@ -0,0 +1,8 @@
+from django.forms import ModelForm
+from veiculo.models import Veiculo
+
+class FormularioVeiculo(ModelForm):
+    class Meta:
+        model = Veiculo
+        exclude = []
+        
--- veiculo/views.py
+++ veiculo/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from veiculo.models import Veiculo
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreatView
+from veiculo.forms import FormularioVeiculo
+from django.views.generic import ListView, CreateView
 
 class ListarVeiculos(ListView):
     model = Veiculo
     context_object_name = 'veiculos'
     template_name = 'veiculo/listar.html'
 
-class CriarVeiculos (CreatView):
+class CriarVeiculos (CreateView):
     model = Veiculo
+    form_class = FormularioVeiculo
     template_name = 'veiculo/novo.html'
-    sucess_url = reverse_lazy('listar-veiculos')
+    success_url = reverse_lazy('listar-veiculos')
