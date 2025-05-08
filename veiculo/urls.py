d76b9a6769269b7ba4e26ad2d5fcf73aee922adf
Heloisa Rolins <heloisarolins@gmail.com>
2025-05-07 21:38:41 -0300

    finalizando aula do dia 3

--- veiculo/urls.py
+++ veiculo/urls.py
@@ -4,4 +4,5 @@ from veiculo.views import *
 urlpatterns = [
     path ('',ListarVeiculos.as_view(), name = 'listar-veiculos'),
     path ('novo/', CriarVeiculos.as_view(), name='criar-veiculos'),
+    path ('fotos/<str:arquivo>/', FotoVeiculo.as_view(), name='foto-veiculo'),
 ]
--- veiculo/views.py
+++ veiculo/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from veiculo.models import Veiculo
+from django.views.generic import View
 from django.urls import reverse_lazy
 from veiculo.forms import FormularioVeiculo
+from django.http import FileResponse, Http404
+from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import ListView, CreateView
 
 class ListarVeiculos(ListView):
@@ -14,3 +17,13 @@ class CriarVeiculos (CreateView):
     form_class = FormularioVeiculo
     template_name = 'veiculo/novo.html'
     success_url = reverse_lazy('listar-veiculos')
+
+class FotoVeiculo (View):
+    def get (self, request, arquivo):
+        try:
+            veiculo = Veiculo.objects.get (foto='veiculo/fotos/{}'.format(arquivo))
+            return FileResponse (veiculo.foto)
+        except ObjectDoesNotExist:
+            raise Http404 ("Foto não encontrada ou acesso não autorizado")
+        except Exception as exception:
+            raise exception 
