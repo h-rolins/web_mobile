824a4825a635ce20d852be5b16587786fb4bdccd
Heloisa Rolins <heloisarolins@gmail.com>
2025-05-28 21:25:16 -0300

    parte final

--- sistema/urls.py
+++ sistema/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from sistema.views import Login, Logout
+from sistema.views import Login, Logout, LoginAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Login.as_view(), name='login'),
+    path ('autenticacao-api/',LoginAPI.as_view()),
     path('logout/', Logout.as_view(), name='logout'),
     path('veiculo/', include('veiculo.urls'), name='veiculo'),
     path('anuncio/',include('anuncio.urls'), name ='anuncio')
--- sistema/views.py
+++ sistema/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic import View
 from django.contrib.auth import authenticate, login, logout
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 
 class Login (View):
     def get (self, request):
@@ -23,6 +27,25 @@ class Login (View):
 
         print(usuario)
         print(senha)
+
+class LoginAPI(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={
+                'request': request
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'id': user.id,
+            'nome': user.first_name,
+            'email': user.email,
+            'token': token.key
+        })
 
 class Logout (View):
     def get(self, request):
